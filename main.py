ae4f11a0a5d9373077c2b351d6eebc97466a689a
ASmallBurger <kartik0503@gmail.com>
2025-08-08 20:25:47 +0530

    Made text more clear

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from ui import run_app
+import sys
 
+
+if sys.platform == "win32":
+    try:
+        import ctypes
+        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    except Exception:
+        pass
 if __name__ == "__main__":
     run_app()
